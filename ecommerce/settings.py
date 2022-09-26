9e4d855dc2bff63d5b3fe0dab72dd30c922b7745
muhammad-ahmad-qbatch <muhammad.ahmad@qbatch.com>
2022-09-26 15:08:42 +0500

    :sparkles:

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'store',
-    'gunicorn' 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,3 @@
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings.dev')
+# import os
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings.dev')
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
